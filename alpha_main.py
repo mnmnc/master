1416ec65f8e3b2f535d014f220e4032945fbd30b
mnmnc <chooseright+git@gmail.com>
2014-12-22 12:53:32 +0100

    Multiple dev environments force me to temporarily create a separate execution based on detected os.

--- alpha_main.py
+++ alpha_main.py
@@ -4,7 +4,8 @@ from ftools import pcap_csv as p2c
 from plotter import plotter
 from arrtools import arrtools as arr
 import random
-import matplotlib
+import os
+
 
 
 def process_protocol(protocol, tshark_path, pcap_file, csv_file, skip_pcap=False):
@@ -47,56 +48,20 @@ def process_protocol(protocol, tshark_path, pcap_file, csv_file, skip_pcap=False
 	return headers, data
 
 
-def main():
-	# INIT
-	random.seed()
-
+def testing():
+	# TESTING
 	# INPUT
-	input_directory = "D:\\Poligon\\input\\"
-	input_directory = "E:\\SQL_PROJECTS\\Poligon\\input\\"
 	input_directory = "/home/x/projects/poligon/input/"
-
 	input_file = "smaller_00000_20120316133000.pcap"
-	input_file2 = "smaller_00001_20120316133058.pcap"
-	input_file3 = "smaller_00002_20120316134254.pcap"
-	input_file4 = "smaller_00003_20120316134407.pcap"
-	input_file5 = "1.pcap"
 
 	# OUTPUT
-	output_directory = "D:\\Poligon\\output\\"
-	output_directory = "E:\\SQL_PROJECTS\\Poligon\\output\\"
 	output_directory = "/home/x/projects/poligon/output/"
 	output_file = "test1.csv"
 	image_output_name = "plotted"
 	image_output_format = ".png"
 
 	# OTHER VARIABLES
-	tshark_path = "D:\\Apps\\Wireshark\\tshark.exe"
-	tshark_path = "C:\\tshark.exe"
 	tshark_path = "tshark"
-
-
-	headers, data = process_protocol("tcp", tshark_path, input_directory + input_file4, output_directory + output_file, False)
-	selected_data = arr.select_from_data(data, [headers[0], headers[2]])
-	print(selected_data)
-
-	xs = []
-	ys = []
-
-	for row in selected_data:
-		x = -1
-		y = -1
-		try:
-			x = float(row[0])
-			y = float(row[1])
-		except:
-			pass
-		if x is not -1 and y is not -1:
-			xs.append(float(row[0]))
-			ys.append(float(row[1]))
-
-
-	# TESTING
 	if True == False:
 		protocols = ["frame", "ip", "tcp", "udp", "dns", "icmp"]
 		for protocol in protocols:
@@ -104,56 +69,73 @@ def main():
 			headers, data = process_protocol(protocol, tshark_path, input_directory + input_file4, output_directory + output_file, False)
 			print(headers)
 
-	print(xs)
-	print(ys)
 
-	# BUILDING 2D DATA
-	#xs, ys = build_2d(data, "sport", "deport")
+def work_main():
+
+	# INPUT
+	input_directory = "D:\\Poligon\\input\\"
+	input_file = "smaller_00000_20120316133000.pcap"
+
+	# OUTPUT
+	output_directory = "D:\\Poligon\\output\\"
+	output_file = "test.csv"
+	image_output_name = "plotted"
+	image_output_format = ".png"
+
+	# OTHER VARIABLES
+	tshark_path = "D:\\Apps\\Wireshark\\tshark.exe"
+
+	# EXECUTION
+	headers, data = process_protocol("tcp", tshark_path, input_directory + input_file, output_directory + output_file, False)
 
 	# BUILDING [ number of packets per (source port) x (destination port) ]
 	#xs, ys, sizes = build_2d_with_number_of_packets_per_pair(data, "sport", "deport")
 
-	# PLOTTING DATA
-
-	plotter.set_axis_limit(65535,33)
-	# PLOTTING DATA WITH MARKER SIZES
-	#plotter.set_title("Porty przeznaczenia i liczba pakietów jako wielkość markera")
-	plotter.plot(xs, ys, "circle", "r", 0.4)
-	#plotter.plot_with_sizes(xs, ys, sizes, "circle", "r", 0.4)
-
-	# with open(output_directory + "l.log", "w") as f:
-	# 	for i in range(len(xs)):
-	# 		f.write(str(xs[i]) + "-" + str(ys[i]) + " -> " + str(sizes[i]) + "\n")
-
-	with open(output_directory + "log.txt", "w") as f:
-		ux = []
-		uy = []
-		for i in range(len(xs)):
-			if xs[i] not in ux:
-				ux.append(xs[i])
-			if ys[i] not in uy:
-				uy.append(ys[i])
-
-		for i in range(len(uy)):
-			f.write(str(uy[i]) + "\n")
-
-	# SAVING IMAGE
-
-	#plotter.save_img(output_directory + image_output_name + image_output_format)
-	plotter.set_label("x", headers[0])
-	plotter.set_label("y", headers[2])
-	plotter.set_text(3000, 1, "FIN")
-	plotter.set_text(5000, 2, "SYN")
-	plotter.set_text(3000, 4, "RST")
-	plotter.set_text(3000, 8, "PSH")
-	plotter.set_text(3000, 16, "ACK")
-	plotter.set_text(3000, 32, "URG")
+	# plotter.set_title("Porty przeznaczenia i liczba pakietów jako wielkość markera")
+	# plotter.plot(xs, ys, "circle", "r", 0.4)
+	# plotter.plot_with_sizes(xs, ys, sizes, "circle", "r", 0.4)
+	# plotter.save_img_csize(output_directory + image_output_name + image_output_format, 10, 5, 200)
 
 
+def linux_main():
 
+	# INPUT
+	input_directory = "/home/x/projects/poligon/input/"
+	input_file = "smaller_00000_20120316133000.pcap"
 
+	# OUTPUT
+	output_directory = "/home/x/projects/poligon/output/"
+	output_file = "test1.csv"
+	image_output_name = "plotted"
+	image_output_format = ".png"
 
-	plotter.save_img_csize(output_directory + image_output_name + image_output_format, 10, 5, 200)
+	# OTHER VARIABLES
+	tshark_path = "tshark"
+
+	# EXECUTION
+	headers, data = process_protocol("tcp", tshark_path, input_directory + input_file, output_directory + output_file, False)
+
+	# BUILDING [ number of packets per (source port) x (destination port) ]
+	#xs, ys, sizes = build_2d_with_number_of_packets_per_pair(data, "sport", "deport")
+
+	# plotter.set_title("Porty przeznaczenia i liczba pakietów jako wielkość markera")
+	# plotter.plot(xs, ys, "circle", "r", 0.4)
+	# plotter.plot_with_sizes(xs, ys, sizes, "circle", "r", 0.4)
+	# plotter.save_img_csize(output_directory + image_output_name + image_output_format, 10, 5, 200)
+
+def main():
+	# INIT
+	random.seed()
+
+	# OS CONDITIONAL EXECUTION / PART OF THE DEVELOPMENT PROCESS.
+	# WILL BE REMOVED FOR FINAL VERSION
+	if os.name == "nt":
+		work_main()
+	elif os.name == "posix":
+		linux_main()
+	else:
+		print("[ERR] OS not supported. \n\tI give up...")
+		return
 
 
 if __name__ == "__main__":
